6a768decda27c10692dcc78cd536610de9a2bc4e
unknown <lukasz.jalowiecki@gmail.com>
2019-09-08 11:13:39 +0200

    init project

---
+++ src/main/java/com/kodilla/ecarsfront/EcarsFrontApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.ecarsfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcarsFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcarsFrontApplication.class, args);
+    }
+
+}
